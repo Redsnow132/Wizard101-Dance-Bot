5573567bf7231e78abba778f48e7b38189b9d3d9
Redsnow132 <nancedarian@gmail.com>
2016-03-13 16:35:18 -0400

    Update Data.cs

--- Data.cs
+++ Data.cs
@@ -1,10 +1,40 @@
 ##Format
 game1{
-pattern1:up,down,down
-pattern2:down,up,up,up
+pattern1:right,down,left
+pattern2:up,left,right,left
+pattern3:right,down,down,down,down
+pattern4:down,down,up,right,left,up
+pattern5:down,left,right,right,right,down,up
 }
 
 game2{
-pattern1:up,down,down
-pattern2:down,up,up,up
+pattern1:left,up,down
+pattern2:up,up,left,left
+pattern3:down,right,left,up,up
+pattern4:right,down,left,down,left,up
+pattern5:right,down,left,down,left,up,up
+}
+
+game3{
+  pattern1:down,left,right
+  pattern2:right,up,left,right
+  pattern3:down,down,down,left,up
+  pattern4:right,left,left,left,left,right
+  pattern5:up,up,down,left,left,up,right
+}
+
+game4{
+  pattern1:up,left,right
+  pattern2:left,right,right,up
+  pattern3:right,right,down,down,up
+  pattern4:down,up,up,right,up,down
+  pattern5:down,left,left,left,up,up,up
+}
+
+game5{
+  pattern1:down,left,right
+  pattern2:down,down,up,up
+  pattern3:right,right,right,down,down
+  pattern4:right,right,right,left,left,left
+  pattern5:left,right,down,right,left,up,down
 }
