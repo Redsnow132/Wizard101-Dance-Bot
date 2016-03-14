7fc9e4b086c7bd58fda420ee4c8fd76fdd73aa33
Redsnow132 <nancedarian@gmail.com>
2016-03-13 21:57:53 -0400

    Update Data.cs

--- Data.cs
+++ Data.cs
@@ -1,10 +1,131 @@
-##Format
-game1{
-pattern1:up,down,down
-pattern2:down,up,up,up
+ game1{
+ pattern1:up,down,down
+ pattern2:down,up,up,up
+ pattern1:right,down,left
+ pattern2:up,left,right,left
+ pattern3:right,down,down,down,down
+ pattern4:down,down,up,right,left,up
+ pattern5:down,left,right,right,right,down,up
+ }
+  
+game2{
+ pattern1:up,down,down
+ pattern2:down,up,up,up
+ pattern1:left,up,down
+ pattern2:up,up,left,left
+ pattern3:down,right,left,up,up
+ pattern4:right,down,left,down,left,up
+ pattern5:right,down,left,down,left,up,up
 }
 
-game2{
-pattern1:up,down,down
-pattern2:down,up,up,up
+game3{
+  pattern1:down,left,right
+  pattern2:right,up,left,right
+  pattern3:down,down,down,left,up
+  pattern4:right,left,left,left,left,right
+  pattern5:up,up,down,left,left,up,right
+}
+
+game4{
+  pattern1:up,left,right
+  pattern2:left,right,right,up
+  pattern3:right,right,down,down,up
+  pattern4:down,up,up,right,up,down
+  pattern5:down,left,left,left,up,up,up
+}
+
+game5{
+  pattern1:down,left,right
+  pattern2:down,down,up,up
+  pattern3:right,right,right,down,down
+  pattern4:right,right,right,left,left,left
+  pattern5:left,right,down,right,left,up,down
+  }
+  
+game6{
+  pattern1:right,down,down
+  pattern2:up,left,up,up
+  pattern3:up,down,right,up,up
+  pattern4:right,left,down,right,up,right
+  pattern5:up,up,right,up,right,left,up
+}
+
+game7{
+  pattern1:down,down,up
+  pattern2:down,up,up,right
+  pattern3:right,up,right,right,left
+  pattern4:up,up,down,up,up,down
+  pattern5:right,left,left,down,left,left,right
+}
+
+game8{
+  pattern1:down,left,left
+  pattern2:down,down,down,down
+  pattern3:left,right,left,up,left
+  pattern4:up,down,right,right,right,right
+  pattern5:right,up,right,up,down,down,left
+}
+
+game9{
+  pattern1:left,down,left
+  pattern2:up,down,down,left
+  pattern3:right,down,down,up,right
+  pattern4:right,right,right,left,down,down
+  pattern5:up,left,down,right,right,up,right
+}
+
+game10{
+  pattern1:left,down,up
+  pattern2:up,up,right,down
+  pattern3:right,right,right,down,right
+  pattern4:right,up,up,left,left,down
+  pattern5:up,left,down,right,right,left,down
+}
+
+game11{
+  pattern1:up,right,down
+  pattern2:down,down,down,left
+  pattern3:down,up,down,left,down
+  pattern4:up,up,left,left,left,left
+  pattern5:down,left,up,down,down,left,left
+}
+
+game12{
+  pattern1:up,down,left
+  pattern2:right,right,right,up
+  pattern3:left,down,up,left,right
+  pattern4:up,down,right,up,up,up
+  pattern5:left,left,right,up,down,down,down
+}
+
+game13{
+  pattern1:right,right,down
+  pattern2:down,up,up,right
+  pattern3:down,right,down,down,left
+  pattern4:up,right,up,down,right,up
+  pattern5:left,down,left,down,up,down,left
+}
+
+game14{
+  pattern1:down,right,right
+  pattern2:right,down,right,right
+  pattern3:left,left,up,up,right
+  pattern4:down,left,down,up,up,up
+  pattern5:left,down,right,right,up,right,up
+}
+
+game15{
+  pattern1:up,right,right
+  pattern2:left,left,up,right
+  pattern3:left,left,up,right,down
+  pattern4:right,left,down,down,down,up
+  pattern5:down,right,left,down,down,right,right
+}
+
+game16{
+  pattern1:up,down,left
+  pattern2:down,down,right,up
+  pattern3:down,up,left,up,left
+  pattern4:right,right,left,up,left,up
+  pattern5:down,left,right,down,right,down,left
 }
